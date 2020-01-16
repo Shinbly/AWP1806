c8979ad4bd7dfa25d1c8175cea2e5c55c0376845
Shinbly <jasonfachan@live.fr>
2020-01-16 20:06:28 +0700

    edit UI

--- Scrumz/src/Pages/Board.js
+++ Scrumz/src/Pages/Board.js
@@ -4,13 +4,25 @@ import Paper from '@material-ui/core/Paper';
 
 import { makeStyles } from '@material-ui/core/styles';
 
+import Backlog from '../Assets/BacklogExemple.json'
+import SprintBacklog from '../Assets/SprintBacklog.json'
+import Todo from '../Assets/Todo.json'
+import Doing from '../Assets/Doing.json'
+import ToTest from '../Assets/ToTest.json'
+import Done from '../Assets/Done.json'
+import { Button } from '@material-ui/core';
+
 const useStyles = makeStyles(theme => ({
-  paper: {
-    height: 140,
-    width: 100,
+  colomn: {
+    width: 170,
+  },
+  task: {
+    paddingBlock : 10,
+    backgroundColor : "lighblue",
+    width: 145,
   },
   control: {
-      padding: theme.spacing(2),
+      padding: theme.spacing(4),
   },
 }));
 
@@ -20,9 +32,31 @@ function Board() {
   return (
       <Grid item xs={12}>
         <Grid container justify="center" spacing={spacing} >
-          {[0, 1, 2, 3, 4, 5].map(value => (
+          {[Backlog, SprintBacklog, Todo, Doing, ToTest, Done].map(value => (
             <Grid key={value} item>
-              <Paper className={classes.paper}/>
+              <Paper className={classes.colomn}>
+                <h4>
+                  {value.name}
+                </h4>
+                <Grid container justify="center" spacing={spacing}>
+                  {value.tasks.map(task => (
+                    <Paper elevation={6} className={classes.task}>
+                      <h4>
+                          {task.name}
+                      </h4>
+                      {task.description}
+                     </Paper>
+                  ))}
+                </Grid>
+                {(value.name == "Backlog")
+                ?
+                <Button>
+                  +Add a Card
+                </Button>
+                :
+                null}
+              </Paper>
+
             </Grid>
           ))}
         </Grid>
