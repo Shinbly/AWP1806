89578dd8cc2d615610dbb807369b5496c9ab1711
Eulalie <eulalie.gaquer@gmail.com>
2020-04-26 12:57:34 +0200

    interface update

--- Scrumz/client/src/Pages/Login.js
+++ Scrumz/client/src/Pages/Login.js
@@ -84,9 +84,6 @@ const styles = theme => ({
 			<div className={classes.root}>
                 <AppBar position="static">
                     <Toolbar>
-                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                            <MenuIcon />
-                        </IconButton>
                         <Typography variant="h6" className={classes.title}>
                             Scrumz
               					</Typography>
